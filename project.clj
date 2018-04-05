44ed5f118368b6c5e93264a9445a2b096e67578d
Erwin Rossen <erwin@nyon.nl>
2018-04-05 11:28:24 +0200

    Restructuring of Clojure demo project

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject clojure_demo "0.1.0-SNAPSHOT"
-  :description "Clojure for the Brave and True"
-  :url "https://www.braveclojure.com"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot clojure-demo.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
